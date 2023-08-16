89e7d2a7e65a50bd19512ae3bb7d5e7b4a47a6e0
DeepanshuVishwakarma <108495451+DeepanshuVishwakarma@users.noreply.github.com>
2023-08-16 21:03:41 +0530

    first

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,14 +8,17 @@ export default function Home() {
   const [foodItems, setFoodItems] = useState([]);
   const [search, setSearch] = useState("");
   const loadFoodItems = async () => {
-    let response = await fetch("http://localhost:5000/api/auth/foodData", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    let response = await fetch(
+      "https://binfood-backend.onrender.com/api/auth/foodData",
+      {
+        // credentials: 'include',
+        // Origin:"http://localhost:3000/login",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     response = await response.json();
     // console.log(response[1][0].CategoryName)
     setFoodItems(response[0]);
